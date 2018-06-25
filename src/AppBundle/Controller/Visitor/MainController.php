0093df0b5cd0c4fc234071c88db09faeea889b05
NicoRizz16 <nicolas.rizzon@gmail.com>
2018-06-25 21:06:52 +0200

    add suggest QCM

--- src/AppBundle/Controller/Visitor/MainController.php
+++ src/AppBundle/Controller/Visitor/MainController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller\Visitor;
 
+use AppBundle\Entity\Qcm;
+use AppBundle\Form\SuggestQcmType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -37,6 +39,31 @@ class MainController extends Controller
         $user = $this->getUser();
         return $this->render(':visitor/main:_user_nav.html.twig', array(
             'user' => $user
+        ));
+    }
+
+    /**
+     * @Route("/qcm/proposer", name="suggest_qcm")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function suggestQcmAction(Request $request)
+    {
+        $qcm = new Qcm();
+        $form = $this->createForm(SuggestQcmType::class, $qcm);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $qcm->setPublished(false);
+            $qcm->setAuthor($this->getUser());
+            $em->persist($qcm);
+            $em->flush();
+
+            return $this->render(':visitor/main:suggest_qcm_sent.html.twig');
+        }
+
+        return $this->render(':visitor/main:suggest_qcm.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
